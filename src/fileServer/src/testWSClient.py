51027aa53fee66460e2500d53f22cc6c55dfb3a9
killuh <20297127+killuhwhale@users.noreply.github.com>
2024-05-10 11:55:35 -0700

    wss

--- src/fileServer/src/testWSClient.py
+++ src/fileServer/src/testWSClient.py
@@ -22,8 +22,10 @@ async def run():
     ssl_context = ssl.create_default_context()
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
+    print("Connecting...")
     async with websockets.connect("wss://localhost:4000", ssl=ssl_context) as ws:
     # async with websockets.connect("wss://heyjamieai.com/webrtcwss/", ssl=ssl_context) as ws:
+        print("Connected...")
         while True:
             await ws.send(json.dumps({
                 "partyName": sys.argv[1],

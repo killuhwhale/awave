85249fcd4340c287c6e9c6316a1bec7c76633ab4
killuh <20297127+killuhwhale@users.noreply.github.com>
2024-06-05 17:54:09 -0700

    do nothing on back button

--- src/appControlCenter/main.py
+++ src/appControlCenter/main.py
@@ -29,6 +29,7 @@ chrome_options.add_argument('--overscroll-history-navigation=disabled')
 # Initialize the Chrome driver @
 chrome_driver_path = "/opt/chromedriver"
 caps = webdriver.DesiredCapabilities.CHROME.copy()
+caps['overscroll-history-navigation'] = False
 print(f"{caps=}")
 service = ChromeService(executable_path=chrome_driver_path, desired_capabilities=caps)
 driver = webdriver.Chrome(service=service, options=chrome_options)

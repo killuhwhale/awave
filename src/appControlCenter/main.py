ee06d63791a0b5f25d2d03d9c95cb194c98e0060
killuh <20297127+killuhwhale@users.noreply.github.com>
2024-06-05 17:24:24 -0700

    do nothing on back button

--- src/appControlCenter/main.py
+++ src/appControlCenter/main.py
@@ -23,6 +23,8 @@ chrome_options.add_argument("--kiosk")  # Full-screen mode without toolbars or U
 chrome_options.add_argument("--disable-pinch")  #
 chrome_options.add_argument("--overscroll-history-navigation=0")  #overscroll history navigation
 chrome_options.add_argument('--disable-features=TouchpadOverscrollHistoryNavigation')
+chrome_options.add_argument('--overscroll-history-navigation=disabled')
+
 
 
 
